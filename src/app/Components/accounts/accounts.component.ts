86792c6cfdc22102f0d9bba5f3f85794706dea41
marinasamir15 <marinasamir15102001@gmail.com>
2025-05-05 21:09:46 +0300

    add export and import accounts

--- src/app/Components/accounts/accounts.component.ts
+++ src/app/Components/accounts/accounts.component.ts
@@ -4,6 +4,9 @@ import { AccountService } from '../../shared/services/account.service';
 import { CommonModule } from '@angular/common';
 import { AccountnodeComponent } from '../accountnode/accountnode.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { Modal } from 'bootstrap';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
 
 interface Account {
   id: string;
@@ -17,7 +20,7 @@ interface Account {
 @Component({
   selector: 'app-accounts',
   standalone: true,
-  imports: [CommonModule, AccountnodeComponent,TranslateModule],
+  imports: [CommonModule, AccountnodeComponent,TranslateModule,FormsModule,ReactiveFormsModule],
   templateUrl: './accounts.component.html',
   styleUrls: ['./accounts.component.css']
 })
@@ -27,6 +30,11 @@ export class AccountsComponent implements OnInit {
   parentAccounts: Account[] = [];
   hierarchicalAccounts: Account[] = [];
   type: string | null = '';
+  isSubmitted=false;
+  isLoading =false;
+  accountImportForm: FormGroup = new FormGroup({
+    file: new FormControl(null, [Validators.required]),
+  });
   constructor(private _AccountService: AccountService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -64,4 +72,78 @@ export class AccountsComponent implements OnInit {
       child: account.child ? this.buildAccountHierarchy(account.child) : []
     }));
   }
+  exportAccounts() {
+    this._AccountService.exportAccountsData().subscribe({
+      next: (response) => {
+        const blob = new Blob([response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'accounts.xlsx'); 
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      },
+      error: (err) => {
+        console.error("Error downloading file:", err);
+      }
+    });
+  }
+  openModal(modalId: string) {
+    const modalElement = document.getElementById(modalId);
+    if (modalElement) {
+      const modal = new Modal(modalElement);
+      modal.show();
+    }
+  }
+  closeModal(modalId: string) {
+    const modalElement = document.getElementById(modalId);
+   this.accountImportForm.patchValue({file:null});  
+    if (modalElement) {
+      const modal = Modal.getInstance(modalElement);
+      modal?.hide();
+    }
+  }
+
+  onFileColorSelect(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.accountImportForm.patchValue({ file: file }); 
+      console.log(file)
+      this.accountImportForm.get('file')?.updateValueAndValidity();
+    }else{
+      console.error('No file selected');
+    }
+    
+  }
+  handleForm(){
+    this.isSubmitted =true;
+      if (this.accountImportForm.valid) {
+          this.isLoading = true;
+
+    const formData = new FormData();
+    const file = this.accountImportForm.get('file')?.value;
+
+    if (file instanceof File) { 
+      formData.append('file', file, file.name);
+    } else {
+      console.error('Invalid file detected:', file);
+      return; 
+    }
+    this._AccountService.importAccountsData(formData).subscribe({
+      next: (response) => {
+        console.log(response);
+        if (response) {
+          this.isLoading = false; 
+          this.closeModal('ImportForm');       
+        }
+      },
+      error: (err: HttpErrorResponse) => {
+        this.isLoading = false;
+         
+      }
+    });
+  }
+      }
+
 }
