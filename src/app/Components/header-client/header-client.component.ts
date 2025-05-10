3d89d9706190dbac0e5f4bc3a877bc0d27b2989b
marinasamir15 <marinasamir15102001@gmail.com>
2025-05-10 19:46:08 +0300

    add changes

--- src/app/Components/header-client/header-client.component.ts
+++ src/app/Components/header-client/header-client.component.ts
@@ -4,7 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-header-client',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink],
   templateUrl: './header-client.component.html',
   styleUrl: './header-client.component.css'
 })
