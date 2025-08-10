3819477c6916c6054aaf396e616a3d3ced4b0a1a
marinasamir15 <marinasamir15102001@gmail.com>
2025-08-10 18:48:32 +0300

    add change in navbar

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -5,7 +5,6 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../shared/services/auth.service';
 import { HttpErrorResponse } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -15,18 +14,24 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class HeaderComponent implements OnInit {
   isLanguageDropdownOpen = false;
+  isUserDropdownOpen = false; // New property for user dropdown
   isLoggedIn: boolean = false;
   user_name: string = '';
+  user_image: string = ""
   iconPairs = [
-        { old: 'fa-angle-left', new: 'fa-angle-right' },
-        { old: 'fa-chevron-left', new: 'fa-chevron-right' } ];
-  //currentIconDirection = 'right'; // Default to right-facing icon
+    { old: 'fa-angle-left', new: 'fa-angle-right' },
+    { old: 'fa-chevron-left', new: 'fa-chevron-right' }
+  ];
   currentLang: string | undefined;
 
-  constructor(private translate: TranslateService,  private _Router: Router,private _AuthService: AuthService) {
-    this.currentLang= localStorage.getItem('lang') || 'ar';
-
+  constructor(
+    private translate: TranslateService,  
+    private _Router: Router,
+    private _AuthService: AuthService
+  ) {
+    this.currentLang = localStorage.getItem('lang') || 'ar';
   }
+
   ngOnInit(): void {
     const token = localStorage.getItem('Token');
     this.isLoggedIn = !!token; 
@@ -34,11 +39,21 @@ export class HeaderComponent implements OnInit {
     if (token) {
       this.getMyInfo(); 
     }
+
+    // Close dropdown when clicking outside
+    document.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (!target.closest('.dropdown')) {
+        this.isUserDropdownOpen = false;
+      }
+    });
   }
+
   getMyInfo() {
     this._AuthService.getMyInfo().subscribe({
       next: (response) => {
         this.user_name = response.data.user.user_name;
+        this.user_image = response.data.user.image || ''; // Handle case where user_image might be null
         console.log(response);
       },
       error: (err: HttpErrorResponse) => {
@@ -48,18 +63,20 @@ export class HeaderComponent implements OnInit {
       }
     });
   }
+
   toggleLanguageDropdown() {
     this.isLanguageDropdownOpen = !this.isLanguageDropdownOpen;
+  }
+
+  toggleUserDropdown() {
+    this.isUserDropdownOpen = !this.isUserDropdownOpen;
   }
 
   switchLanguage(lang: string) {
     this.currentLang = lang;
     this.translate.use(lang);
-    //this.updateIcons();
     this.setLanguageDirection(lang);
     this.isLanguageDropdownOpen = false;
-    //this.iconSwitcherService.switchIcons(lang, this.iconPairs);
-
   }
 
   setLanguageDirection(lang: string) {
@@ -68,31 +85,30 @@ export class HeaderComponent implements OnInit {
       htmlElement.dir = 'rtl';
       htmlElement.lang = 'ar';
       localStorage.setItem('lang', 'ar');
-
-
     } else {
       htmlElement.dir = 'ltr';
       htmlElement.lang = lang;
       localStorage.setItem('lang', lang);
-
     }
   }
 
   logout() {
     const formData = new FormData();
     formData.append('user_name', this.user_name);   
+    
     this._AuthService.logout(formData).subscribe({
       next: (res) => {
         console.log('Logged out:', res);
         localStorage.removeItem('Token');
         this.isLoggedIn = false;
+        this.isUserDropdownOpen = false; // Close dropdown
         this._Router.navigate(['/login']);
       },
       error: (err: HttpErrorResponse) => {
         console.error('Logout failed:', err);
-    
         localStorage.removeItem('Token');
         this.isLoggedIn = false;
+        this.isUserDropdownOpen = false; // Close dropdown
         this._Router.navigate(['/login']);
       }
     });
